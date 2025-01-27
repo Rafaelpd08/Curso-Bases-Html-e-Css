aaa8196ff86b337945120a888ac5799cbb4a12c1
Rafael Pereira <rafaelpd08@outlook.com>
2025-01-27 17:24:09 -0300

    Projeto finalizado

--- src/JS/index.js
+++ src/JS/index.js
@@ -22,7 +22,21 @@ const btnVoltar = document.getElementById("btn-voltar");
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
 
-//- passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
+cartoes.forEach(cartao => {
+  cartao.addEventListener("click", function() {
+    const cartaVirada = cartao.querySelector(".carta-virada");
+
+    // virar o cartão
+    cartao.classList.toggle("virar");
+    // mostrar o fundo da carta
+    cartaVirada.classList.toggle("mostrar-fundo-carta");
+
+    const descricao = cartao.querySelector(".descricao");
+    descricao.classList.toggle("esconder");
+  });
+});
+
+
 btnAvancar.addEventListener("click", function () {
   if (cartaoAtual === cartoes.length - 1) return;
 
